06f7aca71bfba500f2677b037e404a83c1e3081c
Oscar Bastardo <osalbr.14@gmail.com>
2024-01-20 12:57:43 +0000

    integrating income, bills and expenses with savings and investments

---
+++ saving_pots/data/D2312.py
@@ -0,0 +1,39 @@
+INPUTS = {
+    "savings": {
+        "ulster_bank": {"oscar": 10000, "rose": 0},
+        "hampshire_trust_bank": {"oscar": 0, "rose": 23600},
+    },
+    "income": {
+        "salary": {"oscar": 4900, "rose": 2542},
+        "investments": {"oscar": 0, "rose": 0},
+    },
+    "bills": {
+        "mortgage": {"oscar": 1973.17, "rose": 0},
+        "electricity": {"oscar": 0, "rose": 150},
+        "gas": {"oscar": 0, "rose": 168.72},
+        "water": {"oscar": 0, "rose": 78.87},
+        "council_tax": {"oscar": 0, "rose": 188.0},
+        "internet": {"oscar": 60, "rose": 0},
+        "home_insurance": {"oscar": 0, "rose": 23.19},
+    },
+    "expenses": {
+        "shopping": {"oscar": 130.68, "rose": 318.36},
+        "freya": {"oscar": 71.48, "rose": 113.61},
+        "cleaning": {"oscar": 81.40, "rose": 0},
+        "personal": {"oscar": 0, "rose": 0},
+    },
+}
+
+PARAMS = {
+    "pots": {
+        "new_house": 0.3,
+        "current_house": 0.15,
+        "holiday": 0.15,
+        "wedding": 0.10,
+        "car": 0.10,
+        "investment": 0.10,
+        "children": 0.05,
+        "family": 0.05,
+    },
+    "split": "proportional_to_income",
+}
